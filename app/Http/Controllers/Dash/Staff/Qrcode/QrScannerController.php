2a58424de6dc65437ef38c0e027d2f91849a2140
A. A. Sumitro <hello@aasumitro.id>
2021-10-02 17:11:37 +0800

    #13 cache id

--- app/Http/Controllers/Dash/Staff/Qrcode/QrScannerController.php
+++ app/Http/Controllers/Dash/Staff/Qrcode/QrScannerController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers\Dash\Staff\Qrcode;
 
 use App\Http\Controllers\Controller;
 use App\Models\Device;
+use Illuminate\Support\Facades\Cache;
 
 class QrScannerController extends Controller
 {
     public function index()
     {
-        $device_unique_id = Device::where([
-            'department_id' => auth()->user()->profile->department_id,
-            'display' => 'DASHBOARD',
-        ])->first()->unique_id;
+        $department_id = auth()->user()->profile->department_id;
+
+        $device_unique_id = Cache::remember(
+            "duid_dash_$department_id", 120,
+            function () use ($department_id) {
+                return Device::where([
+                    'department_id' => $department_id,
+                    'display' => 'DASHBOARD',
+                ])->first()->unique_id;
+            });
 
         return view('pages.staff.qrcode.scanner.index', compact('device_unique_id'));
     }

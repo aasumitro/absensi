49d7931903a4130fb38d89c24daeeda4cfa3eff3
A. A. Sumitro <hello@aasumitro.id>
2021-10-29 15:26:09 +0800

    init some job for future update

---
+++ app/Jobs/RemoveUnsedAttachmentJob.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class RemoveUnsedAttachmentJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // TODO
+        // load all attachments
+        // foreach
+        // check relation one by one
+        // if none/unused
+        // remove from storage and database.
+        // execute this every week
+    }
+}
---
+++ app/Jobs/TakeDownSliderWhenExpiredJob.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class TakeDownSliderWhenExpiredJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // TODO :
+        // Load All Slider and foreach all
+        // Check Expired Date one by one
+        // If expired_data not null
+        // Check status
+        // if Show update and change to Hide
+        // execute this every day
+    }
+}
